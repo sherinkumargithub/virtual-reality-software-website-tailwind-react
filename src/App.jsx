6c72067dca6cad270fc60368172fee12c2abbbfd
sherinkumargithub <sherindev404@gmail.com>
2024-11-13 17:32:35 +0530

    first---commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className="text-7xl font-bold text-red-300">
+        Hello world!
+       </h1>
+    </>
+  )
+}
+
+export default App
